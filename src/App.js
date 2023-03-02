9c2b4b55d522fef3139e8febd01a9bc8e0b44541
darkobas <marko@datafund.io>
2023-03-02 22:36:38 +0100

    fix: set homepage

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { BrowserRouter } from "react-router-dom";
 
 const App = () => {
   return (
-    <BrowserRouter basename="/apps/opfs">
+    <BrowserRouter>
       <div className="App" style={{ display: "flex" }}>
         <DemoFSBrowser name={"DemoFS"} id="DemoFS" />
       </div>
